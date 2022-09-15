a8bf67a62587974b0ababf4041583cfe01c5dea4
kristianswile <101113409+kristianswile@users.noreply.github.com>
2022-09-15 15:36:50 +0100

    Initial commit from Create Turborepo

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-interface Props { title: string }
-export const Button = ({ title }: Props) => {
-  return <button>{title}</button>;
+export const Button = () => {
+  return <button>Boop</button>;
 };
